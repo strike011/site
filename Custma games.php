46a8a268e7b304c43ca650419e6e3a6f6f3457b6
strike011 <militom1l3r@gmail.com>
2024-05-27 19:40:05 -0300

    digitando a pagina ninjafrog

---
+++ Custma games.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint maiores necessitatibus tenetur maxime! In omnis temporibus quos deserunt distinctio corporis nisi inventore, neque ipsam minima ipsum adipisci aut voluptas unde.</p>
+    
+</body>
+</html>
---
+++ ninjafrog.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pixelmania015</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <h1>
+        <strong>Ninja Frog</strong>
+    </h1>
+    <div class="container">
+        <div class="nav">
+            <div class="nav-bar">
+                <iframe src="games/index.html" frameborder="0"></iframe>
+
+            </div>
+            
+        </div>
+        
+
+    </div>
+    
+    <button><strong>Jogue Agora</strong></button>
+    <div class="box">
+        <p>jogos retro</p>
+        <h2>
+           ninja frog
+        </h2>
+        <p>
+            
+            Ninja Frog: Uma Aventura Anfíbia Inesquecível!
+            Mergulhe em um mundo vibrante de plataformas e desafios com Ninja Frog, o sapo ninja mais ágil do pântano!
+
+            Enfrente perigos e explore paisagens exuberantes:
+
+            Salte, corra e nade por florestas exuberantes, cavernas misteriosas e vulcões fumegantes.
+            Domine a arte do ninjutsu para superar obstáculos traiçoeiros e inimigos implacáveis.
+            Desvende segredos ancestrais e enfrente o malvado Rei pato em batalhas épicas.
+            Personalize seu sapo ninja:
+
+            Escolha entre diferentes skins e habilidades para se adequar ao seu estilo de jogo.
+            Colecione itens poderosos para aumentar sua força, velocidade e agilidade.
+            Torne-se o ninja supremo e domine a arte do sapo!
+            Ninja Frog oferece:
+
+            Níveis desafiadores e gratificantes com vários caminhos para explorar.
+            Controles intuitivos e fáceis de aprender, perfeitos para jogadores de todas as idades.
+            Gráficos pixelados charmosos e trilha sonora envolvente.
+            Uma história divertida e envolvente que vai te prender do início ao fim.
+            Seja rápido, seja ágil, seja Ninja Frog!
+
+            Disponível agora para [plataformas]
+
+            
+
+            Jogue Ninja Frog e prepare-se para uma aventura épica! <br><br>
+            <h2>Como jogar Ninja Frog</h2>
+            <p>
+                Você joga este jogo no pixelmania015 usando o teclado:
+
+            Seta para esquerda/direita - Mover para esquerda/direita
+            Seta para cima - Pular
+            Seta para cima duas vezes  - salto duplo
+            
+            </p>
+            <h2>
+                Você pode jogar Ninja Frog online gratuitamente em um computador?
+                
+            </h2>
+            <p> 
+                Sim! Você pode jogar gratuitamente em seu navegador sem precisar baixá-lo. Jogue Nija frog usando teclado e mouse. Você pode até acessar o modo de tela cheia no seu PC.
+            </p>
+            <h2>Quem criou Ninja Frog ?</h2>
+            <p>
+                Nija Frog foi desemvolvido por Everton Renan na cidade de campo mourão, com o auxili dos professores da universidade grupointegrado <a href="https://www.grupointegrado.br/" taget="blank" rel="noopener noreferrer"></a>
+                <p>inegrado</p>
+                
+            </p>
+            <h2>
+                Quais personagens estão no Ninja frog 
+            </h2>
+            <p>
+                jogador: sapo nija <br>
+                chefão: pato <br>
+                obstaculos: serras <br>
+                coletar: tomates <br>
+
+
+            </p>
+
+        </p>
+
+    </div>
+</body>
+</html>
