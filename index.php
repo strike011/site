3b6cf92d6b24486ebca11c341e5908a5635b0181
strike011 <militom1l3r@gmail.com>
2024-06-12 22:20:10 -0300

    adicionando bootstrap

--- index.php
+++ index.php
@@ -4,173 +4,173 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pixelmania015</title>
+    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css">
+    <script href="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
+    <script href="bootstrap-5.3.3/dist/js/bootstrap.js"></script>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="shortcut icon" href="imagens/controle-de-video-game.png">
     <meta name="google-adsense-account" content="ca-pub-3069212009847136">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3069212009847136"
-     crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/4746542bc6.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <header class="header">
-        <a href="index.html" title="pagina inicial" class="header-logo"> 
-            <img src="imagens/Inserir um título (2).png" alt="pixelmania015">
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3069212009847136"
-                crossorigin="anonymous"></script>
-        <!-- teste anuncio -->
-        <ins class="adsbygoogle"
-                style="display:block;"
-                data-ad-client="ca-pub-3069212009847136"
-                data-ad-slot="7107818226"
-                
-                data-ad-format="auto"
-                data-full-width-responsive="true"></ins>
-        <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+        <a href="index.html" title="pagina inicial" class="header-logo">
+            <img src="./imagens/pixel.png" alt="pixelmania015">
         </a>
-              <a href="javascript:mostrarmenu()" class="menu">
-              <i class="fa-solid fa-bars"></i> 
-              </a>       
+        <a href="javascript:mostrarmenu()" class="menu">
+            <i class="fa-solid fa-bars"></i>
+        </a>
         <nav class="header-nav">
-        
+
             <ul>
-                <li><a href="index.html"title='home'><strong>Home</strong></a></li>
-                <li><a href="sobre.html"title='sobre'><strong>Sobre</strong></a></li>
-                <li><a href="contato.html"title='contatos'><strong>Contato</strong></a></li>
+                <li><a href="index.html" title='home'><strong>Home</strong></a></li>
+                <li><a href="sobre.html" title='sobre'><strong>Sobre</strong></a></li>
+                <li><a href="contato.html" title='contatos'><strong>Contato</strong></a></li>
             </ul>
 
         </nav>
-        
+
 
     </header>
+
     <section class="banner">
-        <img src="" alt=""class='banner-img'>
+        <img src="" alt="" class='banner-img'>
+        <!--<img src="./imagens/background-main.jpg" alt="" class="imgMain">-->
+        <main class="container">
+            
+            <h1><strong>Jogos em Destaque</strong></h1>
+            <div class="flex">
+                <div class="flex-card">
+                    <a>
+                        <a href="games/index.html"title="imagem" target="_blank">
+                        <img src="imagens/ninja frog.jpeg" alt="imagem" title="jogar agora">
+                    </a>
+                    <p>
+
+                        <strong>Ninja Frog</strong>
+                    </p>
+                    <p>
+                        <a href="ninjafrog.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+                <div class="flex-card">
+                    <a>
+                           
+                        <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem" title="jogar agora">
+                       
+                    </a>
+
+                    <p>
+                        <strong>jogos2</strong>
+                    </p>
+                    <p>
+                        <a href="detalhes.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+                <div class="flex-card">
+                    <a>
+                        <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem" title="jogar agora">
+                    </a>
+                    <p>
+                        <strong>jogos 3</strong>
+                    </p>
+                    <p>
+                        <a href="detalhes.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+                <div class="flex-card">
+                    <a>
+                        <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
+                    </a>
+                    <p>
+                        <strong>jogos 4</strong>
+                    </p>
+                    <p>
+                        <a href="detalhes.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+                <div class="flex-card">
+                    <a>
+                        <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
+                    </a>
+                    <p>
+                        <strong>gamess</strong>
+                    </p>
+                    <p>
+                        <a href="detalhes.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+                <div class="flex-card">
+                    <a>
+                        <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
+                    </a>
+                    <p>
+                        <strong>games</strong>
+                    </p>
+                    <p>
+                        <a href="detalhes.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+                <div class="flex-card">
+                    <a>
+                        <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
+                    </a>
+                    <p>
+                        <strong>games</strong>
+                    </p>
+                    <p>
+                        <a href="detalhes.php" title="detalhes" class="btn">
+                            detalhes
+                        </a>
+                    </p>
+
+                </div>
+        </main>
     </section>
-    <main class="container">
-        <h1><strong>Jogos em Destaque</strong></h1>
-        <div class="flex">
-            <div class="flex-card">
-                <a>
-                  <img src="imagens/ninja frog.jpeg" alt="imagem">
-                </a>
-                <p>
-                    
-                    <strong>Ninja Frog</strong>
-                </p>
-                <p>
-                    <a href="ninjafrog.php" title="detalhes" class="btn">
-                        detalhes 
-                    </a>
-                </p>
-
-            </div>
-            <div class="flex-card">
-                <a>
-                    <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
-                </a>
-               
-                <p>
-                    <strong>jogos2</strong>
-                </p>
-                <p>
-                    <a href="detalhes.php" title="detalhes" class="btn">
-                        detalhes
-                    </a>
-                </p>
-
-            </div>
-            <div class="flex-card">
-                <a>
-                    <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
-                </a>
-                <p>
-                    <strong>jogos 3</strong>
-                </p>
-                <p>
-                    <a href="detalhes.php" title="detalhes" class="btn">
-                        detalhes
-                    </a>
-                </p>
-
-            </div>
-            <div class="flex-card">
-                <a>
-                    <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
-                </a>
-                <p>
-                    <strong>jogos 4</strong>
-                </p>
-                <p>
-                    <a href="detalhes.php" title="detalhes" class="btn">
-                        detalhes
-                    </a>
-                </p>
-
-            </div>
-            <div class="flex-card">
-                <a>
-                    <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
-                </a>
-                <p>
-                    <strong>gamess</strong>
-                </p>
-                <p>
-                    <a href="detalhes.php" title="detalhes" class="btn">
-                        detalhes
-                    </a>
-                </p>
-
-            </div>
-            <div class="flex-card">
-                <a>
-                    <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
-                </a>
-                <p>
-                    <strong>games</strong>
-                </p>
-                <p>
-                    <a href="detalhes.php" title="detalhes" class="btn">
-                        detalhes
-                    </a>
-                </p>
-
-            </div>
-            <div class="flex-card">
-                <a>
-                    <img src="imagens/WhatsApp Image 2024-05-09 at 19.26.53.jpeg" alt="imagem">
-                </a>
-                <p>
-                    <strong>games</strong>
-                </p>
-                <p>
-                    <a href="detalhes.php" title="detalhes" class="btn">
-                        detalhes
-                    </a>
-                </p>
-
-            </div>
-
-        </div>
-
-    </main>
-    
     <footer class='footer-img'>
-        <img src="imagens/logo60.png" alt="pixelmania015">
-       <strong><p class="rodape"> Pereira 670 - Campo Mourão - PR</p></strong> 
+
+        <strong>
+            <p class="rodape">av irmaos Pereira 670 - Campo Mourão - PR</p>
+        </strong>
     </footer>
+
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3069212009847136"
-     crossorigin="anonymous"></script>
-     <script>
-        function mostrarmenu(){
+        crossorigin="anonymous"></script>
+    <script>
+        function mostrarmenu() {
             document.querySelector(".header-nav").classList.toggle("mostrar");
 
         }
-     </script>
-     
-    
+    </script>
+
+
 </body>
+
 </html>
